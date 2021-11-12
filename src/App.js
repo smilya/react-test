a99e952e5fb36f2d29b8ed98051dd2fadbf1f4a2
Ilya Smirnov <ilya_smirnov@epam.com>
2021-11-12 18:32:50 +0300

    local state implementation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
+import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
 import Button from './components/Button';
 import Content from './components/Content';
 
-import initialContent  from './redux/initialContent';
+import initialContent from './redux/initialContent';
 
 function App() {
+  const [content, setContent] = useState(initialContent);
+  const [path, setPath] = useState('');
+  const [newValue, setNewValue] = useState('');
+
+  function getNewContent(oldContent, codeString) {
+    const content = [...oldContent];
+    eval(codeString)
+    return content;
+  }
+
   return (
     <div className="App">
       <div className="header">
-        <InputField label="Путь" />
-        <InputField label="Новое значение" />
-        <Button caption="Применить"></Button>
+        <InputField
+          label="Путь"
+          onChange={e => {
+            setPath(e.target.value);
+          }}
+        />
+        <InputField
+          label="Новое значение"
+          onChange={e => {
+            setNewValue(e.target.value);
+          }}
+        />
+        <Button caption="Применить" onClick={() => {
+          const codeString = `${path}=${newValue}`;
+          const newContent = getNewContent(content, codeString);
+          setContent(newContent)
+        }} />
       </div>
       <div className="content">
-        <Content content={initialContent} />
-       </div>
+        <Content content={content} />
+      </div>
     </div>
   );
 }
