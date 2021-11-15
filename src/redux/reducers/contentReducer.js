b23da30f7507ff1a3261a0c7e066b8c50840f07c
Ilya <smilya13@gmail.com>
2021-11-15 13:29:05 +0300

    correct UPDATE_CONTENT case

--- src/redux/reducers/contentReducer.js
+++ src/redux/reducers/contentReducer.js
@@ -1,11 +1,12 @@
 import initialContent from "../initialContent";
+import { deepClone } from "../assets";
 
 const contentReducer = (oldContent = initialContent, action) => {
   switch (action.type) {
     case "UPDATE_CONTENT":
       const clearPath = action.payload.path.replace(/['"]/g, "");
       const codeString = `${clearPath}=${action.payload.newValue}`;
-      let content = [...oldContent];
+      let content = deepClone(oldContent);
       try {
         eval(codeString);
       } finally {
